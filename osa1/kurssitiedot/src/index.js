045a620faa2559c98675b035a16d2f03889d96b8
Jari Silvo <jari.silvo@cs.helsinki.fi>
2019-07-09 16:00:01 +0300

    kurssitiedot_final

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -5,18 +5,19 @@ const Header = (props) => {
 	return (
 		<>
 		  <h1>
-			{props.course}
+			{props.course.name}
 		  </h1>	
 		</>
 	)
 }
 
 const Content = (props) => {
+	console.log(props)
 	return (
 		<>
-			<Part parts={props.parts[0]}/>
-			<Part parts={props.parts[1]}/>
-			<Part parts={props.parts[2]}/>
+		  <Part parts={props.parts[0]}/>
+		  <Part parts={props.parts[1]}/>
+		  <Part parts={props.parts[2]}/>
 		</>
 	)
 }
@@ -44,28 +45,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  
-  const parts = [
-    {
-		name: 'Fundamentals of React',
-		exercises: 10
-	},
-	{
-		name: 'Using props to pass data',
-		exercises: 7
-	},
-	{
-		name: 'State of a component',
-		exercises: 14
-	}
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-	  <Total parts={parts}/>
+	  <Content parts={course.parts}/>
+	  <Total parts={course.parts}/>
     </div>
   )
 }

6d1e64f9fb434aa19d3c15aced352da6d1872327
Jari Silvo <jari.silvo@cs.helsinki.fi>
2019-08-19 11:24:36 +0300

    Puhelinluettelo 2.6 step1 kesken

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  
+  const addName = (event) => {
+	  event.preventDefault()
+	  console.log('button clicked', event.target)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+
+}
+
+export default App

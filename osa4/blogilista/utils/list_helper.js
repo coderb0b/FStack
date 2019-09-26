d343c256c2e8bd0c64bed290776609e309e9caba
Jari Silvo <jari.silvo@cs.helsinki.fi>
2019-09-26 22:27:58 +0300

    4.6 step4 valmis

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,18 +1,41 @@
 const dummy = (blogs) => {
-	return 1
+  return 1
 }
 
 
 const totalLikes = (blogs) => {
-	const reducer = (sum, item) => {
-		return sum + item
-	}
-	
-	return blogs.reduce(reducer, 0)
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    return (prev.likes > current.likes) ? prev : current
+  }
+  return blogs.reduce(reducer, 0)
+}
+
+const mostBlogs = (blogs) => {
+  const counts = {}
+  blogs.forEach(element => {
+    counts[element.author] = 1 + (counts[element.author] || 0)
+  })
+
+  const name = Object.keys(counts).reduce((a,b) => counts[a] > counts[b] ? a : b)
+  const obj = {
+    author: name,
+    blogs: counts[name]
+  }
+  return obj
 }
 
 
 module.exports = {
-	dummy
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
 

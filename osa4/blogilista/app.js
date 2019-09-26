b09e8e07a349d9a9fc724f0d864f1b878cbdfd4e
Jari Silvo <jari.silvo@cs.helsinki.fi>
2019-09-26 15:03:58 +0300

    Blogilista 4.2 step2 valmis

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,38 +4,24 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+console.log('connecting to', config.MONGODB_URI)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+	  console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+	  console.log('error connection to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('build'))
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+//middlewaret
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = app

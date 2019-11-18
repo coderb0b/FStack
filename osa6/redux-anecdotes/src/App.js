ae0e46341c778b7d914e5e4b8f592f819cac065a
Jari Silvo <jari.silvo@cs.helsinki.fi>
2019-11-18 11:51:19 +0200

    6.4 Anekdootit, step2 valmis

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -4,6 +4,7 @@ import { createAnecdote } from './reducers/anecdoteReducer'
 
 const App = (props) => {
   const anecdotes = props.store.getState()
+  
 
 
   
@@ -20,7 +21,7 @@ const App = (props) => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.sort((a,b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}

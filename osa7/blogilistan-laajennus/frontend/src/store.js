8c41dc6f43d79f778be4168d19dea1019ebe64e6
Jari Silvo <jari.silvo@cs.helsinki.fi>
2019-12-03 16:25:42 +0200

    7.5 Redux, step2 kesken

--- osa7/blogilistan-laajennus/frontend/src/store.js
+++ osa7/blogilistan-laajennus/frontend/src/store.js
@@ -1,15 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import blogReducer from './reducers/blogReducer'
 import notificationReducer from './reducers/notificationReducer'
 
-/*
+
 const reducer = combineReducers({
-    anecdotes: anecdoteReducer,
-    notification: notificationReducer,
-    filter: filterReducer
+    blogs: blogReducer,
+    notification: notificationReducer
   })
-*/
-  const store = createStore(notificationReducer, applyMiddleware(thunk))
+
+  const store = createStore(reducer, applyMiddleware(thunk))
 
   export default store
   
